885f544c259238baf73ace8b08f859b8a1f372c3
steveluscher <me+github@steveluscher.com>
2022-06-03 21:11:29 -0700

    feat: a demo app with which you can obtain a mobile wallet's authorization

--- js/packages/fakewebdapp/pages/_app.tsx
+++ js/packages/fakewebdapp/pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css';
+
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { NativeWalletAdapter } from '@solana/wallet-adapter-mobile';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { clusterApiUrl } from '@solana/web3.js';
 import type { AppProps } from 'next/app';
+import { useMemo } from 'react';
+
+const DEVNET_ENDPOINT = clusterApiUrl(WalletAdapterNetwork.Devnet);
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    const wallets = useMemo(
+        () => [
+            new NativeWalletAdapter({
+                appIdentity: { name: 'Fake Web dApp' },
+            }),
+        ],
+        [],
+    );
+    return (
+        <ConnectionProvider endpoint={DEVNET_ENDPOINT}>
+            <WalletProvider wallets={wallets}>
+                <Component {...pageProps} />
+            </WalletProvider>
+        </ConnectionProvider>
+    );
 }
 
 export default MyApp;
--- js/packages/fakewebdapp/pages/index.tsx
+++ js/packages/fakewebdapp/pages/index.tsx
@@ -1,7 +1,31 @@
+import { WalletName } from '@solana/wallet-adapter-base';
+import { useWallet } from '@solana/wallet-adapter-react';
 import type { NextPage } from 'next';
+import React, { useEffect } from 'react';
+
+import Button from '../components/Button';
 
 const Home: NextPage = () => {
-    return <h1>TODO</h1>;
+    const { connect, connected, publicKey, select, wallet } = useWallet();
+    useEffect(() => {
+        select('Native' as WalletName);
+    }, [select]);
+    if (wallet == null) {
+        return null;
+    }
+    return (
+        <>
+            {connected ? <h1>Public key: {publicKey?.toBase58()}</h1> : null}
+            <Button
+                disabled={connected}
+                onClick={() => {
+                    connect();
+                }}
+            >
+                Authorize
+            </Button>
+        </>
+    );
 };
 
 export default Home;
