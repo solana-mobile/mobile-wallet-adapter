a71915d76d282925f3b566e7b0c6d0a2fff02089
steveluscher <me+github@steveluscher.com>
2022-06-09 18:59:54 -0700

    feat: add an authorization cache to the demo app

--- js/packages/fakewebdapp/pages/_app.tsx
+++ js/packages/fakewebdapp/pages/_app.tsx
@@ -6,6 +6,7 @@ import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react
 import { clusterApiUrl } from '@solana/web3.js';
 import type { AppProps } from 'next/app';
 import { useMemo } from 'react';
+import { AuthorizationResult } from '@solana/mobile-wallet-protocol';
 
 const DEVNET_ENDPOINT = clusterApiUrl(WalletAdapterNetwork.Devnet);
 
@@ -14,6 +15,21 @@ function MyApp({ Component, pageProps }: AppProps) {
         () => [
             new NativeWalletAdapter({
                 appIdentity: { name: 'Fake Web dApp' },
+                authorizationResultCache: {
+                    async get() {
+                        try {
+                            return JSON.parse(
+                                localStorage.getItem('myapp:cachedAuthorizationResult') as string,
+                            ) as AuthorizationResult;
+                        } catch {}
+                    },
+                    async set(authorizationResult: AuthorizationResult) {
+                        localStorage.setItem('myapp:cachedAuthorizationResult', JSON.stringify(authorizationResult));
+                    },
+                    async clear() {
+                        localStorage.removeItem('myapp:cachedAuthorizationResult');
+                    },
+                },
             }),
         ],
         [],
