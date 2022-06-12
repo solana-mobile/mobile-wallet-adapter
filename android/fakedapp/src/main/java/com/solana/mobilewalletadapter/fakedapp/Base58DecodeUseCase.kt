b38f597c80aeecc22d2ff771f616b6c0669c0131
Steven Laver <steven.laver@solana.com>
2022-06-12 15:17:44 -0700

    FakeDapp should send valid transactions, and verify the results from the wallet

---
+++ android/fakedapp/src/main/java/com/solana/mobilewalletadapter/fakedapp/Base58DecodeUseCase.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 Solana Labs, Inc.
+ */
+
+package com.solana.mobilewalletadapter.fakedapp
+
+object Base58DecodeUseCase {
+    // throws IllegalArgumentException if base58 input string contains a non-base58 character
+    operator fun invoke(base58: String): ByteArray {
+        // Max output size is the same length as the input data (for a base58 string of all '1's)
+        val maxDecodedSize = base58.length
+        val decoded = ByteArray(maxDecodedSize)
+
+        // Convert from base58 alphabet characters to the corresponding value
+        val bytes = ByteArray(base58.length) { i ->
+            val c = base58[i].code
+            val b = if (c <= BASE58_ALPHABET_ASCII_LOOKUP.size) BASE58_ALPHABET_ASCII_LOOKUP[c] else -1
+            require(b != (-1).toByte()) { "Character '$c' at [$i] is not a valid base58 character" }
+            b
+        }
+
+        // Skip all leading zeroes; we'll handle these separately at the end
+        var start = 0
+        while (start < bytes.size && bytes[start] == 0.toByte()) {
+            start++
+        }
+        val zeroes = start
+
+        var pos = bytes.size - 1 // NOTE: pos can go as low as -1
+        while (start < bytes.size) {
+            if (bytes[start] == 0.toByte()) {
+                start++
+            } else {
+                var mod = 0
+                for (i in start until bytes.size) {
+                    mod = mod * 58 + bytes[i]
+                    bytes[i] = (mod / 256).toByte()
+                    mod %= 256
+                }
+                decoded[pos--] = mod.toByte()
+            }
+        }
+
+        val result = ByteArray(zeroes + bytes.size - pos - 1)
+        System.arraycopy(decoded, pos + 1, result, zeroes, bytes.size - pos - 1)
+        return result
+    }
+
+    private val BASE58_ALPHABET_ASCII_LOOKUP = byteArrayOf(
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1,  0,  1,  2,  3,  4,  5,  6,  7,  8, -1, -1, -1, -1, -1, -1,
+        -1,  9, 10, 11, 12, 13, 14, 15, 16, -1, 17, 18, 19, 20, 21, -1,
+        22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, -1, -1, -1, -1, -1,
+        -1, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, -1, 44, 45, 46,
+        47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, -1, -1, -1, -1, -1,
+    )
+}
---
+++ android/fakedapp/src/main/java/com/solana/mobilewalletadapter/fakedapp/MemoTransaction.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2022 Solana Labs, Inc.
+ */
+
+package com.solana.mobilewalletadapter.fakedapp
+
+import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters
+import org.bouncycastle.crypto.signers.Ed25519Signer
+
+object MemoTransaction {
+    fun create(publicKeyBase58: String): ByteArray {
+        val transaction = MEMO_TRANSACTION_TEMPLATE.clone()
+        val publicKeyBytes = Base58DecodeUseCase(publicKeyBase58)
+        assert(publicKeyBytes.size == 32) { "Invalid public key length for a Solana transaction" }
+        System.arraycopy(publicKeyBytes, 0, transaction, ACCOUNT_PUBLIC_KEY_OFFSET, publicKeyBytes.size)
+        return transaction
+    }
+
+    fun verify(publicKeyBase58: String, signedTransaction: ByteArray) {
+        val publicKeyBytes = Base58DecodeUseCase(publicKeyBase58)
+        assert(publicKeyBytes.size == 32) { "Invalid public key length for a Solana transaction" }
+
+        // First, check that the provided transaction wasn't mangled by the wallet
+        require(signedTransaction.size == MEMO_TRANSACTION_TEMPLATE.size) { "Unexpected signed transaction size" }
+        val unsignedTransaction = signedTransaction.clone()
+        unsignedTransaction.fill(0, SIGNATURE_OFFSET, HEADER_OFFSET)
+        unsignedTransaction.fill(0, ACCOUNT_PUBLIC_KEY_OFFSET, ACCOUNT_PUBLIC_KEY_OFFSET + 32)
+        require(unsignedTransaction.contentEquals(MEMO_TRANSACTION_TEMPLATE)) { "Signed memo transaction does not match the one sent" }
+        val signedTransactionAccount = signedTransaction.copyOfRange(ACCOUNT_PUBLIC_KEY_OFFSET, ACCOUNT_PUBLIC_KEY_OFFSET + 32)
+        require(signedTransactionAccount.contentEquals(publicKeyBytes)) { "Invalid signing account in transaction" }
+
+        val publicKey = Ed25519PublicKeyParameters(publicKeyBytes, 0)
+        val signer = Ed25519Signer()
+        signer.init(false, publicKey)
+        signer.update(signedTransaction, HEADER_OFFSET, signedTransaction.size - HEADER_OFFSET)
+        val signature = signedTransaction.copyOfRange(SIGNATURE_OFFSET, HEADER_OFFSET)
+        val verified = signer.verifySignature(signature)
+        require(verified) { "Transaction signature is invalid" }
+    }
+
+    // NOTE: the blockhash of this transaction is fixed, and will be too old to actually execute. It
+    // is for test purposes only.
+    private val MEMO_TRANSACTION_TEMPLATE = byteArrayOf(
+        0x01.toByte(), // 1 signature required (fee payer)
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), // First signature (fee payer account)
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+        0x01.toByte(), // 1 signature required (fee payer)
+        0x00.toByte(), // 0 read-only account signatures
+        0x01.toByte(), // 1 read-only account not requiring a signature
+        0x02.toByte(), // 2 accounts
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), // Fee payer account public key
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+        0x05.toByte(), 0x4a.toByte(), 0x53.toByte(), 0x5a.toByte(), 0x99.toByte(), 0x29.toByte(), 0x21.toByte(), 0x06.toByte(), // Memo program v2 account address
+        0x4d.toByte(), 0x24.toByte(), 0xe8.toByte(), 0x71.toByte(), 0x60.toByte(), 0xda.toByte(), 0x38.toByte(), 0x7c.toByte(),
+        0x7c.toByte(), 0x35.toByte(), 0xb5.toByte(), 0xdd.toByte(), 0xbc.toByte(), 0x92.toByte(), 0xbb.toByte(), 0x81.toByte(),
+        0xe4.toByte(), 0x1f.toByte(), 0xa8.toByte(), 0x40.toByte(), 0x41.toByte(), 0x05.toByte(), 0x44.toByte(), 0x8d.toByte(),
+        0xa2.toByte(), 0x53.toByte(), 0x0d.toByte(), 0xb2.toByte(), 0x8d.toByte(), 0x52.toByte(), 0xd1.toByte(), 0x42.toByte(), // Recent blockhash (not really in this case, though)
+        0x9a.toByte(), 0xac.toByte(), 0x32.toByte(), 0xc8.toByte(), 0x6e.toByte(), 0x47.toByte(), 0xd4.toByte(), 0x07.toByte(),
+        0x74.toByte(), 0xe6.toByte(), 0x79.toByte(), 0x3d.toByte(), 0xf1.toByte(), 0xe7.toByte(), 0xf3.toByte(), 0x2b.toByte(),
+        0x69.toByte(), 0xfa.toByte(), 0x4c.toByte(), 0x76.toByte(), 0xfc.toByte(), 0xcb.toByte(), 0xb4.toByte(), 0x01.toByte(),
+        0x01.toByte(), // program ID (index into list of accounts)
+        0x01.toByte(), // 1 account
+        0x00.toByte(), // account index 0
+        0x0b.toByte(), // 11 byte payload
+        0x68.toByte(), 0x65.toByte(), 0x6c.toByte(), 0x6c.toByte(), 0x6f.toByte(), 0x20.toByte(), 0x77.toByte(), 0x6f.toByte(), // "hello world"
+        0x72.toByte(), 0x6c.toByte(), 0x64.toByte(),
+    )
+
+    private const val SIGNATURE_OFFSET = 1
+    private const val HEADER_OFFSET = 65
+    private const val ACCOUNT_PUBLIC_KEY_OFFSET = 69
+}
