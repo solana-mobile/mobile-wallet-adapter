f684c0a73f28666f1acb57584e85285adfc42cfd
Continuous Integration <>
2022-07-03 04:52:20 +0000

    Update common javadoc

---
+++ common/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.solana.mobilewalletadapter.common"},{"l":"com.solana.mobilewalletadapter.common.crypto"},{"l":"com.solana.mobilewalletadapter.common.protocol"},{"l":"com.solana.mobilewalletadapter.common.util"}]
