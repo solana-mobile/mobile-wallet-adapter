12f7436a629ba197e90b36bc1b3abf03914e0f06
Andrew Watson <andrew.watson@solana.com>
2022-07-11 16:05:17 -0700

    Lots more API refinement

--- android/clientlib-ktx/src/main/java/com/solana/mobilewalletadapter/clientlib/MobileWalletAdapter.kt
+++ android/clientlib-ktx/src/main/java/com/solana/mobilewalletadapter/clientlib/MobileWalletAdapter.kt
@@ -1,16 +1,23 @@
 package com.solana.mobilewalletadapter.clientlib
 
 import androidx.activity.result.ActivityResultCaller
-import androidx.activity.result.contract.ActivityResultContracts
-import com.solana.mobilewalletadapter.clientlib.scenario.ScenarioTypes
+import com.solana.mobilewalletadapter.clientlib.scenario.*
 
 class MobileWalletAdapter(
     private val resultCaller: ActivityResultCaller,
-    private val scenarioType: ScenarioTypes = ScenarioTypes.Local
+    private val scenarioType: ScenarioTypes = ScenarioTypes.Local,
+    private val timeout: Int = Scenario.DEFAULT_CLIENT_TIMEOUT_MS
 ) {
+    private val contract = LocalAssociationIntentContract(LocalAssociationIntentCreator())
+    private val resultLauncher = resultCaller.registerForActivityResult(contract) { }
 
-    init {
-        resultCaller.registerForActivityResult(ActivityResultContracts.GetContent()) { }
+    private val scenarioChooser = ScenarioChooser()
+
+    fun getThisStuffReady() {
+        val scenario = scenarioChooser.chooseScenario<LocalAssociationScenario>(scenarioType, timeout)
+        val details = scenario.associationDetails()
+
+        resultLauncher.launch(details)
     }
 
 }
