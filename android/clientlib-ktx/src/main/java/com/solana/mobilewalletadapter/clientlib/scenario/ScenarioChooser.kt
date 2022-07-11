72c4093bfb6f7b1d686939b62ee00616ba53946e
Andrew Watson <andrew.watson@solana.com>
2022-07-11 16:05:17 -0700

    Lots more API refinement

---
+++ android/clientlib-ktx/src/main/java/com/solana/mobilewalletadapter/clientlib/scenario/ScenarioChooser.kt
@@ -0,0 +1,13 @@
+
+class ScenarioChooser {
+
+    @Suppress("UNCHECKED_CAST") //We will assume consumers know what they are doing
+    fun <T : Scenario> chooseScenario(scenarioType: ScenarioTypes, timeout: Int): T {
+        return when (scenarioType) {
+            ScenarioTypes.Local -> {
+                LocalAssociationScenario(timeout) as T
+            }
+        }
+    }
+
+}
