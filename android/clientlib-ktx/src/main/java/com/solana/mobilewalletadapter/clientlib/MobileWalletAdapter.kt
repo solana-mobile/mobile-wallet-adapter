f0ff01aacc48977fc537f262df22749288f4d7e1
Andrew Watson <andrew.watson@solana.com>
2022-07-11 16:26:29 -0700

    Write initial authorize method using new functionality.

--- android/clientlib-ktx/src/main/java/com/solana/mobilewalletadapter/clientlib/MobileWalletAdapter.kt
+++ android/clientlib-ktx/src/main/java/com/solana/mobilewalletadapter/clientlib/MobileWalletAdapter.kt
@@ -1,8 +1,14 @@
 package com.solana.mobilewalletadapter.clientlib
 
+import android.net.Uri
 import androidx.activity.result.ActivityResultCaller
+import com.solana.mobilewalletadapter.clientlib.protocol.MobileWalletAdapterClient
 import com.solana.mobilewalletadapter.clientlib.scenario.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.concurrent.TimeUnit
 
+@Suppress("BlockingMethodInNonBlockingContext")
 class MobileWalletAdapter(
     private val resultCaller: ActivityResultCaller,
     private val scenarioType: ScenarioTypes = ScenarioTypes.Local,
@@ -13,11 +19,26 @@ class MobileWalletAdapter(
 
     private val scenarioChooser = ScenarioChooser()
 
-    fun getThisStuffReady() {
+    private fun associate(): MobileWalletAdapterClient {
+        TODO()
+    }
+
+    suspend fun authorize(identityUri: Uri, iconUri: Uri, identityName: String) {
         val scenario = scenarioChooser.chooseScenario<LocalAssociationScenario>(scenarioType, timeout)
         val details = scenario.associationDetails()
 
         resultLauncher.launch(details)
+
+        val client = scenario.start().get(ASSOCIATION_TIMEOUT_MS, TimeUnit.MILLISECONDS)
+
+        withContext(Dispatchers.IO) {
+            client.authorize(identityUri, iconUri, identityName).get()
+        }
+
+        scenario.close().get(ASSOCIATION_TIMEOUT_MS, TimeUnit.MILLISECONDS)
     }
 
+    companion object {
+        private const val ASSOCIATION_TIMEOUT_MS = 10000L
+    }
 }
