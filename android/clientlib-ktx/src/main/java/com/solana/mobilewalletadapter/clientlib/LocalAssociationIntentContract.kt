2c0cf5e1af58ba950daa01dc57aaab02cd609ac1
Andrew Watson <andrew.watson@solana.com>
2022-07-11 16:05:17 -0700

    Lots more API refinement

--- android/clientlib-ktx/src/main/java/com/solana/mobilewalletadapter/clientlib/LocalAssociationIntentContract.kt
+++ android/clientlib-ktx/src/main/java/com/solana/mobilewalletadapter/clientlib/LocalAssociationIntentContract.kt
@@ -2,18 +2,36 @@ package com.solana.mobilewalletadapter.clientlib
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.activity.result.contract.ActivityResultContract
+import com.solana.mobilewalletadapter.clientlib.protocol.MobileWalletAdapterSession
+import com.solana.mobilewalletadapter.clientlib.scenario.LocalAssociationIntentCreator
 import com.solana.mobilewalletadapter.clientlib.scenario.LocalAssociationScenario
 
 /**
- *
+ * Details necessary to create the association intent
+ */
+data class AssociationDetails(
+    val uriPrefix: Uri? = null,
+    val port: Int,
+    val session: MobileWalletAdapterSession
+)
+
+fun LocalAssociationScenario.associationDetails(uri: Uri? = null) = AssociationDetails(
+    uriPrefix = uri,
+    port = this.port,
+    session = this.session
+)
+
+/**
+ * ActivityResultCaller interface so that we can encapsulate and possibly use in better context in the future
  */
 internal class LocalAssociationIntentContract(
-    private val localScenario: LocalAssociationScenario
-) : ActivityResultContract<Unit, Unit>() {
+    val creator: LocalAssociationIntentCreator
+) : ActivityResultContract<AssociationDetails, Unit>() {
 
-    override fun createIntent(context: Context, input: Unit): Intent {
-        return localScenario.createAssociationIntent()
+    override fun createIntent(context: Context, input: AssociationDetails): Intent {
+        return creator.createAssociationIntent(input.uriPrefix, input.port, input.session)
     }
 
     override fun parseResult(resultCode: Int, intent: Intent?) { }
