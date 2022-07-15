aef9aa0b5b230ab428d3f49da190e574be39b539
Andrew Watson <creativedrewy@gmail.com>
2022-07-15 12:52:40 -0700

    Start getting sample UI to behave the way we want.

--- examples/example-clientlib-ktx-app/app/src/main/java/com/solanamobile/ktxclientsample/activity/MainActivity.kt
+++ examples/example-clientlib-ktx-app/app/src/main/java/com/solanamobile/ktxclientsample/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.solanamobile.ktxclientsample.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,12 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.solana.mobilewalletadapter.clientlib.ActivityResultSender
 import com.solanamobile.ktxclientsample.ui.SampleScreen
 import com.solanamobile.ktxclientsample.ui.theme.KtxClientSampleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), ActivityResultSender {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,9 +24,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    SampleScreen()
+                    SampleScreen(this)
                 }
             }
         }
+    }
+
+    override fun launch(intent: Intent) {
+        startActivityForResult(intent, 0)
     }
 }
--- examples/example-clientlib-ktx-app/app/src/main/java/com/solanamobile/ktxclientsample/ui/SampleScreen.kt
+++ examples/example-clientlib-ktx-app/app/src/main/java/com/solanamobile/ktxclientsample/ui/SampleScreen.kt
@@ -1,50 +1,79 @@
 package com.solanamobile.ktxclientsample.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.solana.mobilewalletadapter.clientlib.ActivityResultSender
 import com.solanamobile.ktxclientsample.viewmodel.SampleViewModel
 
 @Composable
 fun SampleScreen(
+    intentSender: ActivityResultSender,
     viewmodel: SampleViewModel = hiltViewModel()
 ) {
+    val viewState = viewmodel.viewState.collectAsState().value
+
     Column(
         modifier = Modifier.padding(8.dp)
     ) {
         Text(
-            text = "Ktx Clientlib Sample",
+            modifier = Modifier.fillMaxWidth(),
+            text = "Ktx Client Sample",
             style = MaterialTheme.typography.h4
+        )
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                viewmodel.connectToWallet(intentSender)
+            }
+        ) {
+            Text("Connect to Wallet")
+        }
+
+        Divider(
+            modifier = Modifier.padding(
+                top = 16.dp,
+                bottom = 16.dp
+            )
         )
 
         val memoText by remember { mutableStateOf(TextFieldValue("")) }
 
         OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = memoText,
             label = { Text("Memo Text") },
             onValueChange = { }
         )
 
-        Button(onClick = { /*TODO*/ }) {
-            Text("Record Memo")
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            enabled = viewState.isConnected,
+            onClick = { /*TODO*/ }
+        ) {
+            Text("Record Message")
         }
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            enabled = viewState.isConnected,
+            onClick = { /*TODO*/ }
+        ) {
             Text("Sign Message")
         }
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            enabled = viewState.isConnected,
+            onClick = { /*TODO*/ }
+        ) {
             Text("Request Airdrop")
         }
     }
