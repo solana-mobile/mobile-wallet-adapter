336fe7eb01d9ddcdc889a2992e0442b05e0012b8
Andrew Watson <creativedrewy@gmail.com>
2022-07-15 10:27:01 -0700

    Start putting in UI controls

--- examples/example-clientlib-ktx-app/app/src/main/java/com/solanamobile/ktxclientsample/ui/SampleScreen.kt
+++ examples/example-clientlib-ktx-app/app/src/main/java/com/solanamobile/ktxclientsample/ui/SampleScreen.kt
@@ -1,7 +1,18 @@
 package com.solanamobile.ktxclientsample.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.solanamobile.ktxclientsample.viewmodel.SampleViewModel
 
@@ -9,10 +20,32 @@ import com.solanamobile.ktxclientsample.viewmodel.SampleViewModel
 fun SampleScreen(
     viewmodel: SampleViewModel = hiltViewModel()
 ) {
-    Greeting(name = "Ktx Sample")
-}
+    Column(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Text(
+            text = "Ktx Clientlib Sample",
+            style = MaterialTheme.typography.h4
+        )
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+        val memoText by remember { mutableStateOf(TextFieldValue("")) }
+
+        OutlinedTextField(
+            value = memoText,
+            label = { Text("Memo Text") },
+            onValueChange = { }
+        )
+
+        Button(onClick = { /*TODO*/ }) {
+            Text("Record Memo")
+        }
+
+        Button(onClick = { /*TODO*/ }) {
+            Text("Sign Message")
+        }
+
+        Button(onClick = { /*TODO*/ }) {
+            Text("Request Airdrop")
+        }
+    }
 }
