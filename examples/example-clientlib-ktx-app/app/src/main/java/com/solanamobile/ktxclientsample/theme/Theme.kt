b95fa59e694eec140ea1089991704720aa07eba0
Andrew Watson <creativedrewy@gmail.com>
2022-07-26 11:06:16 -0700

    Start making sample app UI look like RN sample

--- examples/example-clientlib-ktx-app/app/src/main/java/com/solanamobile/ktxclientsample/theme/Theme.kt
+++ examples/example-clientlib-ktx-app/app/src/main/java/com/solanamobile/ktxclientsample/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -13,9 +14,9 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xFF6750a4),
+    secondary = Color(0xFFe7e0ec),
+    surface = Color(0xFFe7e0ec),
 
     /* Other default colors to override
     background = Color.White,
--- examples/example-clientlib-ktx-app/app/src/main/java/com/solanamobile/ktxclientsample/ui/SampleScreen.kt
+++ examples/example-clientlib-ktx-app/app/src/main/java/com/solanamobile/ktxclientsample/ui/SampleScreen.kt
@@ -1,15 +1,14 @@
 package com.solanamobile.ktxclientsample.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.VpnKey
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -23,90 +22,140 @@ fun SampleScreen(
 ) {
     val viewState = viewmodel.viewState.collectAsState().value
 
-    Column(
+    Box(
         modifier = Modifier
-            .padding(8.dp)
+            .fillMaxSize()
     ) {
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp),
-            text = "Ktx Client Sample",
-            style = MaterialTheme.typography.h4
-        )
-
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = {
-                if (!viewState.isConnected) {
-                    viewmodel.connectToWallet(intentSender)
-                } else {
-                    viewmodel.disconnect(intentSender)
-                }
-            }
-        ) {
-            Text(
-                text = if (!viewState.isConnected) "Connect to Wallet" else "Disconnect from Wallet"
-            )
-        }
-
-        Box(
-            modifier = Modifier
-                .background(
-                    if (viewState.isConnected) {
-                        Color.Green
-                    } else {
-                        Color.LightGray
-                    }
-                )
-                .fillMaxWidth()
-        ) {
+        Column {
             Text(
                 modifier = Modifier
-                    .fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                text = "Address: ${ viewState.userAddress }"
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colors.surface)
+                    .padding(8.dp),
+                text = "Ktx Client Sample",
+                style = MaterialTheme.typography.h4,
+                textAlign = TextAlign.Center
             )
-        }
 
-        Divider(
-            modifier = Modifier.padding(
-                top = 16.dp,
-                bottom = 16.dp
+            Divider(
+                modifier = Modifier.padding(
+                    top = 16.dp,
+                    bottom = 16.dp
+                )
             )
-        )
 
-        val memoText by remember { mutableStateOf(TextFieldValue("")) }
+            var memoText by remember { mutableStateOf("") }
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = memoText,
-            label = { Text("Memo Text") },
-            onValueChange = { }
-        )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = memoText,
+                label = { Text("Memo Text") },
+                onValueChange = { memoText = it }
+            )
 
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            enabled = viewState.isConnected,
-            onClick = { /*TODO*/ }
-        ) {
-            Text("Record Message")
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                enabled = viewState.isConnected,
+                onClick = { /*TODO*/ }
+            ) {
+                Text("Record Message")
+            }
         }
 
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            enabled = viewState.isConnected,
-            onClick = { /*TODO*/ }
-        ) {
-            Text("Sign Message")
-        }
+//        Button(
+//            modifier = Modifier.fillMaxWidth(),
+//            onClick = {
+//                if (!viewState.isConnected) {
+//                    viewmodel.connectToWallet(intentSender)
+//                } else {
+//                    viewmodel.disconnect(intentSender)
+//                }
+//            }
+//        ) {
+//            Text(
+//                text = if (!viewState.isConnected) "Connect to Wallet" else "Disconnect from Wallet"
+//            )
+//        }
 
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            enabled = viewState.isConnected,
-            onClick = { /*TODO*/ }
+//        Box(
+//            modifier = Modifier
+//                .background(
+//                    if (viewState.isConnected) {
+//                        Color.Green
+//                    } else {
+//                        Color.LightGray
+//                    }
+//                )
+//                .fillMaxWidth()
+//        ) {
+//            Text(
+//                modifier = Modifier
+//                    .fillMaxWidth(),
+//                textAlign = TextAlign.Center,
+//                text = "Address: ${ viewState.userAddress }"
+//            )
+//        }
+
+//        Button(
+//            modifier = Modifier.fillMaxWidth(),
+//            enabled = viewState.isConnected,
+//            onClick = { /*TODO*/ }
+//        ) {
+//            Text("Sign Message")
+//        }
+//
+//        Button(
+//            modifier = Modifier.fillMaxWidth(),
+//            enabled = viewState.isConnected,
+//            onClick = { /*TODO*/ }
+//        ) {
+//            Text("Request Airdrop")
+//        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .background(MaterialTheme.colors.surface)
+                .padding(8.dp)
         ) {
-            Text("Request Airdrop")
+            Row {
+                Text(
+                    text = "Balance: \u25ce 5",
+                    style = MaterialTheme.typography.h5
+                )
+
+                Spacer(Modifier.weight(1f))
+                
+                Button(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(text = "Add Funds")
+                }
+            }
+
+            Row {
+                Icon(
+                    imageVector = Icons.Filled.VpnKey,
+                    contentDescription = "Add Address",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(24.dp)
+                )
+
+                Text(
+                    text = "8hEeWszgrA2XkRK4GH6zL4Qq5wJBwotwsB6VEweD8YEQ"
+                )
+            }
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = "Add funds to enable"
+                )
+            }
         }
     }
 }
