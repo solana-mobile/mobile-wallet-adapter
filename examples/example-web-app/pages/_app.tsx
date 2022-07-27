bef32dbf9ac295125c5ea12cb1857a75cdaef62a
steveluscher <me+github@steveluscher.com>
2022-07-26 18:08:07 -0700

    feat: you can now configure the `SolanaMobileWalletAdapter` with an address selector

--- examples/example-web-app/pages/_app.tsx
+++ examples/example-web-app/pages/_app.tsx
@@ -5,7 +5,11 @@ import { createTheme } from '@mui/material';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { clusterApiUrl } from '@solana/web3.js';
-import { createDefaultAuthorizationResultCache, SolanaMobileWalletAdapter } from '@solana-mobile/wallet-adapter-mobile';
+import {
+    createDefaultAddressSelector,
+    createDefaultAuthorizationResultCache,
+    SolanaMobileWalletAdapter,
+} from '@solana-mobile/wallet-adapter-mobile';
 import type { AppProps } from 'next/app';
 import { SnackbarProvider } from 'notistack';
 import { useMemo } from 'react';
@@ -22,6 +26,7 @@ function ExampleMobileDApp({ Component, pageProps }: AppProps) {
                 ? [] // No wallet adapters when server-side rendering.
                 : [
                       new SolanaMobileWalletAdapter({
+                          addressSelector: createDefaultAddressSelector(),
                           appIdentity: {
                               icon: 'images/app_icon.png',
                               name: 'Mobile Web dApp',
