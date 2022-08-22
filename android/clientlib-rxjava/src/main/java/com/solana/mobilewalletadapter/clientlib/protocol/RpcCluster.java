b379bd702cb83b3c293e3962cc031de447514504
olivierg13 <olivier.goutay@gmail.com>
2022-08-22 16:30:03 +0200

    Adding RpcCluster enum and using it in the authorize API

---
+++ android/clientlib-rxjava/src/main/java/com/solana/mobilewalletadapter/clientlib/protocol/RpcCluster.java
@@ -0,0 +1,23 @@
+package com.solana.mobilewalletadapter.clientlib.protocol;
+
+import androidx.annotation.NonNull;
+
+import com.solana.mobilewalletadapter.common.ProtocolContract;
+
+enum RpcCluster {
+    MAINNET_BETA(ProtocolContract.CLUSTER_MAINNET_BETA),
+    TESTNET(ProtocolContract.CLUSTER_TESTNET),
+    DEVNET(ProtocolContract.CLUSTER_DEVNET);
+
+    @NonNull
+    private final String clusterName;
+
+    RpcCluster(@NonNull String clusterMainnetBeta) {
+        this.clusterName = clusterMainnetBeta;
+    }
+
+    @NonNull
+    public String getClusterName() {
+        return clusterName;
+    }
+}
