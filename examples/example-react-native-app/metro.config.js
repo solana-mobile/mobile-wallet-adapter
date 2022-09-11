f9db6d3db9c76f36b6fde125ecce4f990a3a15b7
steveluscher <me+github@steveluscher.com>
2022-09-10 18:50:57 -0700

    Update the React Native example app to the final release of 0.70.0

--- examples/example-react-native-app/metro.config.js
+++ examples/example-react-native-app/metro.config.js
@@ -5,17 +5,8 @@
  * @format
  */
 
-const defaultSourceExts =
-  require('metro-config/src/defaults/defaults').sourceExts;
-
 /** @type { import("@types/metro-config").ConfigT } */
 module.exports = {
-  resolver: {
-    // Add `.cjs` to allowable extensions. See https://github.com/facebook/metro/issues/535
-    sourceExts: process.env.RN_SRC_EXT
-      ? [...process.env.RN_SRC_EXT.split(',').concat(defaultSourceExts), 'cjs']
-      : [...defaultSourceExts, 'cjs'],
-  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
