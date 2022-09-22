bf42bd1d55c88da50a7435b08c8fc7d018f4a465
Ankur Jain <ankur.jain@solana.com>
2022-09-21 21:22:36 -0700

    Add builder pattern for IdentityRecord class

--- android/walletlib/src/main/java/com/solana/mobilewalletadapter/walletlib/authorization/IdentityRecord.java
+++ android/walletlib/src/main/java/com/solana/mobilewalletadapter/walletlib/authorization/IdentityRecord.java
@@ -30,20 +30,15 @@ public class IdentityRecord {
     @NonNull
     /*package*/ final byte[] secretKeyIV;
 
-    /*package*/ IdentityRecord(int id,
-                               @NonNull String name,
-                               @NonNull Uri uri,
-                               @NonNull Uri relativeIconUri,
-                               @NonNull byte[] secretKeyCiphertext,
-                               @NonNull byte[] secretKeyIV) {
+    /*package*/ IdentityRecord(IdentityRecordBuilder builder) {
         // N.B. This is a package-visibility constructor; these values will all be validated by
         // other components within this package.
-        this.id = id;
-        this.name = name;
-        this.uri = uri;
-        this.relativeIconUri = relativeIconUri;
-        this.secretKeyCiphertext = secretKeyCiphertext;
-        this.secretKeyIV = secretKeyIV;
+        this.id = builder.id;
+        this.name = builder.name;
+        this.uri = builder.uri;
+        this.relativeIconUri = builder.relativeIconUri;
+        this.secretKeyCiphertext = builder.secretKeyCiphertext;
+        this.secretKeyIV = builder.secretKeyIV;
     }
 
     @Override
@@ -68,5 +63,63 @@ public class IdentityRecord {
                 ", uri=" + uri +
                 ", relativeIconUri=" + relativeIconUri +
                 '}';
+    }
+
+    public static class IdentityRecordBuilder {
+        @IntRange(from = 1)
+        private int id;
+
+        @NonNull
+        private String name;
+
+        @NonNull
+        private Uri uri;
+
+        @NonNull
+        private Uri relativeIconUri;
+
+        @NonNull
+        private byte[] secretKeyCiphertext;
+
+        @NonNull
+        private byte[] secretKeyIV;
+
+        public IdentityRecordBuilder() {
+        }
+
+        public IdentityRecordBuilder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public IdentityRecordBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public IdentityRecordBuilder setUri(@NonNull Uri uri) {
+            this.uri = uri;
+            return this;
+        }
+
+        public IdentityRecordBuilder setRelativeIconUri(@NonNull Uri relativeIconUri) {
+            this.relativeIconUri = relativeIconUri;
+            return this;
+        }
+
+        public IdentityRecordBuilder setSecretKeyCiphertext(@NonNull byte[] secretKeyCiphertext) {
+            this.secretKeyCiphertext = secretKeyCiphertext;
+            return this;
+        }
+
+        public IdentityRecordBuilder setSecretKeyIV(@NonNull byte[] secretKeyIV) {
+            this.secretKeyIV = secretKeyIV;
+            return this;
+        }
+
+        public IdentityRecord build() {
+            return new IdentityRecord(this);
+        }
+
     }
 }
