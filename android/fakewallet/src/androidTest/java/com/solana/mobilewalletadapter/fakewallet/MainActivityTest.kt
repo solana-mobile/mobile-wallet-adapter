dca310b7c3ba2d010f23e1a5c5fcb81f4eab9758
funkatronics <funkatronicsmail@gmail.com>
2023-01-24 12:36:52 -0700

    more tests

--- android/fakewallet/src/androidTest/java/com/solana/mobilewalletadapter/fakewallet/MainActivityTest.kt
+++ android/fakewallet/src/androidTest/java/com/solana/mobilewalletadapter/fakewallet/MainActivityTest.kt
@@ -1,7 +1,9 @@
 package com.solana.mobilewalletadapter.fakewallet
 
+import android.net.Uri
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.RootMatchers.isDialog
@@ -11,7 +13,11 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.solana.mobilewalletadapter.clientlib.scenario.LocalAssociationIntentCreator
 import com.solana.mobilewalletadapter.clientlib.scenario.LocalAssociationScenario
 import com.solana.mobilewalletadapter.clientlib.scenario.Scenario
+import com.solana.mobilewalletadapter.common.ProtocolContract
 import com.solana.mobilewalletadapter.walletlib.provider.TestScopeLowPowerMode
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,6 +26,11 @@ import org.junit.runner.RunWith
 class MainActivityTest {
     @get:Rule
     var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Before
+    fun setup() {
+        TestScopeLowPowerMode = false
+    }
 
     @Test
     fun associationIntent_LaunchesAssociationFragment() {
@@ -37,6 +48,102 @@ class MainActivityTest {
         // then
         onView(withId(R.id.associate))
             .check(matches(isDisplayed()))
+    }
+    @Test
+    fun clientAuthRequest_LaunchesAuthorizationFragment() {
+        // given
+        val identityUri = Uri.parse("https://test.com")
+        val iconUri = Uri.parse("favicon.ico")
+        val identityName = "Test"
+        val cluster = ProtocolContract.CLUSTER_TESTNET
+
+        // simulate client side scenario
+        val localAssociation = LocalAssociationScenario(Scenario.DEFAULT_CLIENT_TIMEOUT_MS)
+        val associationIntent = LocalAssociationIntentCreator.createAssociationIntent(
+            null,
+            localAssociation.port,
+            localAssociation.session
+        )
+
+        // when
+        ActivityScenario.launch<MainActivity>(associationIntent)
+
+        // trigger authorization from client
+        localAssociation.start().get().run {
+            authorize(identityUri, iconUri, identityName, cluster)
+        }
+
+        // then
+        onView(withId(R.id.authorize))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun authorizationFlow_SuccessfulAuthorization() {
+        // given
+        val identityUri = Uri.parse("https://test.com")
+        val iconUri = Uri.parse("favicon.ico")
+        val identityName = "Test"
+        val cluster = ProtocolContract.CLUSTER_TESTNET
+
+        // simulate client side scenario
+        val localAssociation = LocalAssociationScenario(Scenario.DEFAULT_CLIENT_TIMEOUT_MS)
+        val associationIntent = LocalAssociationIntentCreator.createAssociationIntent(
+            null,
+            localAssociation.port,
+            localAssociation.session
+        )
+
+        // when
+        ActivityScenario.launch<MainActivity>(associationIntent)
+
+        // trigger authorization from client
+        val authorization = localAssociation.start().get().run {
+            authorize(identityUri, iconUri, identityName, cluster)
+        }
+
+        // then
+        onView(withId(R.id.btn_authorize))
+            .check(matches(isDisplayed())).perform(click())
+
+        val authResult = authorization.get()
+
+        // verify that we got an auth token (successful auth)
+        assertTrue(authResult?.authToken?.isNotEmpty() == true)
+    }
+
+    @Test
+    fun authorizationFlow_DeclinedAuthorization() {
+        // given
+        val identityUri = Uri.parse("https://test.com")
+        val iconUri = Uri.parse("favicon.ico")
+        val identityName = "Test"
+        val cluster = ProtocolContract.CLUSTER_TESTNET
+
+        // simulate client side scenario
+        val localAssociation = LocalAssociationScenario(Scenario.DEFAULT_CLIENT_TIMEOUT_MS)
+        val associationIntent = LocalAssociationIntentCreator.createAssociationIntent(
+            null,
+            localAssociation.port,
+            localAssociation.session
+        )
+
+        // when
+        ActivityScenario.launch<MainActivity>(associationIntent)
+
+        // trigger authorization from client
+        val authorization = localAssociation.start().get().run {
+            authorize(identityUri, iconUri, identityName, cluster)
+        }
+
+        // then
+        onView(withId(R.id.btn_decline))
+            .check(matches(isDisplayed())).perform(click())
+
+        val authResult = kotlin.runCatching { authorization.get() }.exceptionOrNull()
+
+        // verify that we got an auth authorization error (declined auth)
+        assertNotNull(authResult)
     }
 
     @Test
@@ -72,6 +179,44 @@ class MainActivityTest {
         ActivityScenario.launch<MainActivity>(associationIntent)
 
         // then
-        onView(withText(R.string.low_power_mode_warning_title)).inRoot(isDialog()).check(matches(isDisplayed()))
+        onView(withText(R.string.low_power_mode_warning_title))
+            .inRoot(isDialog())
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun lowPowerMode_ShowsWarningAfterInitialConnectionMade() {
+        // given
+        val identityUri = Uri.parse("https://test.com")
+        val iconUri = Uri.parse("favicon.ico")
+        val identityName = "Test"
+        val cluster = ProtocolContract.CLUSTER_TESTNET
+
+        TestScopeLowPowerMode = true
+
+        // simulate client side scenario
+        val localAssociation = LocalAssociationScenario(Scenario.DEFAULT_CLIENT_TIMEOUT_MS)
+        val associationIntent = LocalAssociationIntentCreator.createAssociationIntent(
+            null,
+            localAssociation.port,
+            localAssociation.session
+        )
+
+        // when
+        ActivityScenario.launch<MainActivity>(associationIntent)
+
+        // trigger authorization from client
+        localAssociation.start().get().apply {
+            this.authorize(identityUri, iconUri, identityName, cluster)
+        }
+
+        // click authorize button
+        onView(withId(R.id.btn_authorize))
+            .check(matches(isDisplayed())).perform(click())
+
+        // then
+        onView(withText(R.string.low_power_mode_warning_title))
+            .inRoot(isDialog())
+            .check(matches(isDisplayed()))
     }
 }
