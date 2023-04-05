ec57431304b8764db5559ef8f790533fd4b3b1d9
funkatronics <funkatronicsmail@gmail.com>
2023-04-04 19:58:45 -0600

    fake wallet bottom sheet working, exit anim could be cleaner

--- android/fakewallet/src/main/java/com/solana/mobilewalletadapter/fakewallet/MobileWalletAdapterActivity.kt
+++ android/fakewallet/src/main/java/com/solana/mobilewalletadapter/fakewallet/MobileWalletAdapterActivity.kt
@@ -4,11 +4,13 @@
 
 package com.solana.mobilewalletadapter.fakewallet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -20,7 +22,15 @@ class MobileWalletAdapterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        overridePendingTransition(R.anim.slide_in_from_bottom, R.anim.slide_out_to_bottom)
         setContentView(R.layout.activity_mobile_wallet_adapter)
+
+        val windowLayoutParams = window.attributes
+
+        windowLayoutParams.gravity = Gravity.BOTTOM
+        windowLayoutParams.flags = windowLayoutParams.flags and WindowManager.LayoutParams.FLAG_DIM_BEHIND
+
+        window.attributes = windowLayoutParams
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.CREATED) {
@@ -48,6 +58,11 @@ class MobileWalletAdapterActivity : AppCompatActivity() {
             finish()
             return
         }
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.slide_in_from_bottom, R.anim.slide_out_to_bottom)
     }
 
     companion object {
