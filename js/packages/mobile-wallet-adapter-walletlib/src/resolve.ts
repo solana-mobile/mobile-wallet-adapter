823942707b9f0391731d1c930129779873b6ef2a
michael sulistio <michaelsulistio@gmail.com>
2023-05-15 18:39:31 -0700

    [RN Walletlib] Add resolve(), response types, and hook

---
+++ js/packages/mobile-wallet-adapter-walletlib/src/resolve.ts
@@ -0,0 +1,184 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+    `The package 'solana-mobile-wallet-adapter-walletlib' doesn't seem to be linked. Make sure: \n\n` +
+    '- You rebuilt the app after installing the package\n' +
+    '- If you are using Lerna workspaces\n' +
+    '  - You have added `@solana-mobile/mobile-wallet-adapter-walletlib` as an explicit dependency, and\n' +
+    '  - You have added `@solana-mobile/mobile-wallet-adapter-walletlib` to the `nohoist` section of your package.json\n' +
+    '- You are not using Expo managed workflow\n';
+
+const SolanaMobileWalletAdapterWalletLib =
+    Platform.OS === 'android' && NativeModules.SolanaMobileWalletAdapterWalletLib
+        ? NativeModules.SolanaMobileWalletAdapterWalletLib
+        : new Proxy(
+              {},
+              {
+                  get() {
+                      throw new Error(
+                          Platform.OS !== 'android'
+                              ? 'The package `solana-mobile-wallet-adapter-walletlib` is only compatible with React Native Android'
+                              : LINKING_ERROR,
+                      );
+                  },
+              },
+          );
+
+type AppIdentity = Readonly<{
+    identityUri?: string;
+    iconRelativeUri?: string;
+    identityName?: string;
+}>;
+
+/**
+ * Mobile Wallet Adapter Requests are remote requests coming from
+ * the dApp for authorization, signing, and sending services.
+ */
+
+export type MWARequest =
+    | SignMessagesRequest
+    | SignTransactionsRequest
+    | SignAndSendTransactionsRequest
+    | AuthorizeDappRequest;
+
+export enum MWARequestType {
+    AuthorizeDappRequest,
+    ReauthorizeDappRequest,
+    DeauthorizeDappRequest,
+    SignMessagesRequest,
+    SignTransactionsRequest,
+    SignAndSendTransactionsRequest,
+}
+interface IMWARequest {
+    __type: MWARequestType;
+    requestId: string;
+    sessionId: string;
+    cluster: string;
+    authorizationScope: Uint8Array;
+    appIdentity?: AppIdentity;
+}
+
+export type AuthorizeDappRequest = Readonly<{
+    __type: MWARequestType.AuthorizeDappRequest;
+}> &
+    IMWARequest;
+
+export type ReauthorizeDappRequest = Readonly<{
+    __type: MWARequestType.ReauthorizeDappRequest;
+}> &
+    IMWARequest;
+
+export type DeauthorizeDappRequest = Readonly<{
+    __type: MWARequestType.DeauthorizeDappRequest;
+}> &
+    IMWARequest;
+
+export type SignMessagesRequest = Readonly<{
+    __type: MWARequestType.SignMessagesRequest;
+    payloads: Uint8Array[];
+}> &
+    IMWARequest;
+
+export type SignTransactionsRequest = Readonly<{
+    __type: MWARequestType.SignTransactionsRequest;
+    payloads: Uint8Array[];
+}> &
+    IMWARequest;
+
+export type SignAndSendTransactionsRequest = Readonly<{
+    __type: MWARequestType.SignAndSendTransactionsRequest;
+    payloads: Uint8Array[];
+}> &
+    IMWARequest;
+
+/**
+ * MWA Request Responses
+ */
+
+export type MWAResponse =
+    | AuthorizeDappResponse
+    | SignMessagesResponse
+    | SignTransactionsResponse
+    | SignAndSendTransactionsResponse;
+
+/* Failure Responses */
+export enum MWARequestFailReason {
+    UserDeclined = 'USER_DECLINED',
+    TooManyPayloads = 'TOO_MANY_PAYLOADS',
+    InvalidSignatures = 'INVALID_SIGNATURES',
+    AuthorizationNotValid = 'AUTHORIZATION_NOT_VALID',
+}
+
+export type UserDeclinedResponse = Readonly<{
+    failReason: MWARequestFailReason.UserDeclined;
+}>;
+
+export type TooManyPayloadsResponse = Readonly<{
+    failReason: MWARequestFailReason.TooManyPayloads;
+}>;
+
+export type AuthorizationNotValid = Readonly<{
+    failReason: MWARequestFailReason.AuthorizationNotValid;
+}>;
+
+export type InvalidSignaturesResponse = Readonly<{
+    failReason: MWARequestFailReason.InvalidSignatures;
+    valid: boolean[];
+}>;
+
+/* Authorize Dapp */
+export type AuthorizeDappCompleteResponse = Readonly<{
+    publicKey: Uint8Array;
+    accountLabel?: string;
+    walletUriBase?: string;
+    authorizationScope?: Uint8Array;
+}>;
+export type AuthorizeDappResponse = AuthorizeDappCompleteResponse | UserDeclinedResponse;
+
+/* Sign Transactions/Messages */
+export type SignPayloadsCompleteResponse = Readonly<{ signedPayloads: Uint8Array[] }>;
+export type SignPayloadsFailResponse =
+    | UserDeclinedResponse
+    | TooManyPayloadsResponse
+    | AuthorizationNotValid
+    | InvalidSignaturesResponse;
+
+export type SignTransactionsResponse = SignPayloadsCompleteResponse | SignPayloadsFailResponse;
+export type SignMessagesResponse = SignPayloadsCompleteResponse | SignPayloadsFailResponse;
+
+/* Sign and Send Transaction */
+export type SignAndSendTransactionsCompleteResponse = Readonly<{ signedTransactions: Uint8Array[] }>;
+export type SignAndSendTransactionsResponse =
+    | SignAndSendTransactionsCompleteResponse
+    | UserDeclinedResponse
+    | TooManyPayloadsResponse
+    | AuthorizationNotValid
+    | InvalidSignaturesResponse;
+
+export function resolve(request: AuthorizeDappRequest, response: AuthorizeDappResponse): void;
+export function resolve(request: SignMessagesRequest, response: SignMessagesResponse): void;
+export function resolve(request: SignTransactionsRequest, response: SignTransactionsResponse): void;
+export function resolve(request: SignAndSendTransactionsRequest, response: SignAndSendTransactionsResponse): void;
+export function resolve(request: MWARequest, response: MWAResponse): void {
+    switch (request.__type) {
+        case MWARequestType.AuthorizeDappRequest:
+            // Optionally pre-process the response, which is now nicely typed.
+            SolanaMobileWalletAdapterWalletLib.onResolve(request, response as AuthorizeDappResponse);
+            break;
+        case MWARequestType.SignMessagesRequest:
+            // Optionally pre-process the response, which is now nicely typed.
+            SolanaMobileWalletAdapterWalletLib.onResolve(request, response as SignMessagesResponse);
+            break;
+        case MWARequestType.SignTransactionsRequest:
+            // Optionally pre-process the response, which is now nicely typed.
+            SolanaMobileWalletAdapterWalletLib.onResolve(request, response as SignTransactionsResponse);
+            break;
+        case MWARequestType.SignAndSendTransactionsRequest:
+            // Optionally pre-process the response, which is now nicely typed.
+            SolanaMobileWalletAdapterWalletLib.onResolve(request, response as SignAndSendTransactionsResponse);
+            break;
+        default:
+            console.warn('Unsupported request type');
+            break;
+    }
+}
---
+++ js/packages/mobile-wallet-adapter-walletlib/src/useMobileWalletAdapterSession.ts
@@ -0,0 +1,76 @@
+import { TransactionVersion } from '@solana/web3.js';
+import { useEffect } from 'react';
+import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
+
+import { MWASessionEvent, MWASessionEventType } from './mwaSessionEvents.js';
+import { MWARequest, MWARequestType } from './resolve.js';
+
+const LINKING_ERROR =
+    `The package 'solana-mobile-wallet-adapter-walletlib' doesn't seem to be linked. Make sure: \n\n` +
+    '- You rebuilt the app after installing the package\n' +
+    '- If you are using Lerna workspaces\n' +
+    '  - You have added `@solana-mobile/mobile-wallet-adapter-walletlib` as an explicit dependency, and\n' +
+    '  - You have added `@solana-mobile/mobile-wallet-adapter-walletlib` to the `nohoist` section of your package.json\n' +
+    '- You are not using Expo managed workflow\n';
+
+const SolanaMobileWalletAdapterWalletLib =
+    Platform.OS === 'android' && NativeModules.SolanaMobileWalletAdapterWalletLib
+        ? NativeModules.SolanaMobileWalletAdapterWalletLib
+        : new Proxy(
+              {},
+              {
+                  get() {
+                      throw new Error(
+                          Platform.OS !== 'android'
+                              ? 'The package `solana-mobile-wallet-adapter-walletlib` is only compatible with React Native Android'
+                              : LINKING_ERROR,
+                      );
+                  },
+              },
+          );
+
+const MOBILE_WALLET_ADAPTER_EVENT_BRIDGE_NAME = 'MobileWalletAdapterServiceRequestBridge';
+
+export interface MobileWalletAdapterConfig {
+    supportsSignAndSendTransactions: boolean;
+    maxTransactionsPerSigningRequest: number;
+    maxMessagesPerSigningRequest: number;
+    supportedTransactionVersions: Array<TransactionVersion>;
+    noConnectionWarningTimeoutMs: number;
+}
+
+export function useMobileWalletAdapterSession(
+    walletName: string,
+    config: MobileWalletAdapterConfig,
+    handleRequest: (request: MWARequest) => void,
+    handleSessionEvent: (sessionEvent: MWASessionEvent) => void,
+) {
+    // Start native event listeners
+    useEffect(() => {
+        const mwaEventEmitter = new NativeEventEmitter();
+        mwaEventEmitter.addListener(MOBILE_WALLET_ADAPTER_EVENT_BRIDGE_NAME, (nativeEvent) => {
+            if (isMWARequest(nativeEvent)) {
+                handleRequest(nativeEvent as MWARequest);
+            } else if (isMWASessionEvent(nativeEvent)) {
+                handleSessionEvent(nativeEvent as MWASessionEvent);
+            } else {
+                console.warn('Unexpected native event type');
+            }
+        });
+
+        return () => {
+            mwaEventEmitter.removeAllListeners(MOBILE_WALLET_ADAPTER_EVENT_BRIDGE_NAME);
+        };
+    }, []);
+
+    // Initiate Scenario session with dapp
+    SolanaMobileWalletAdapterWalletLib.startScenario(walletName, config);
+}
+
+function isMWARequest(nativeEvent: any): boolean {
+    return Object.values(MWARequestType).includes(nativeEvent.type);
+}
+
+function isMWASessionEvent(nativeEvent: any) {
+    return Object.values(MWASessionEventType).includes(nativeEvent.type);
+}
