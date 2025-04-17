6b00256662e2f5326ba3dfdcc740594aa01c4ffb
Continuous Integration <>
2025-04-17 20:40:51 +0000

    Update Example Web App

--- example-web-app/_next/static/voSUK_bQYKE-2qYB_1ozU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/571-8fdecd879d9ff606.js","static/chunks/pages/index-2d84e8986e86e5b2.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
