3890d439686d546eeafd71f42a1a446448f90432
funkatronics <funkatronicsmail@gmail.com>
2025-08-18 10:33:55 -0600

    remove comment

--- examples/example-react-native-app/context/ChainContext.tsx
+++ examples/example-react-native-app/context/ChainContext.tsx
@@ -17,8 +17,6 @@ export const DEFAULT_CHAIN_CONFIG = Object.freeze({
     solanaExplorerClusterName: 'devnet',
     solanaRpcSubscriptionsUrl: devnet('wss://api.devnet.solana.com'),
     solanaRpcUrl: devnet('https://api.devnet.solana.com'),
-    // solanaRpcSubscriptionsUrl: devnet('wss://devnet.helius-rpc.com/?api-key=cae9abbb-fbed-4e5e-918a-7e1241d63f1d'),
-    // solanaRpcUrl: devnet('https://devnet.helius-rpc.com/?api-key=cae9abbb-fbed-4e5e-918a-7e1241d63f1d'),
 });
 
 export const ChainContext = createContext<ChainContext>(DEFAULT_CHAIN_CONFIG);
