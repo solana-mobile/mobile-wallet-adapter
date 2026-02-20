1df2c269a7526514995b7de604fdfd4de4e9fb38
funkatronics <funkatronicsmail@gmail.com>
2026-02-20 01:31:20 +0000

    migrate example rn apps to pnpm

--- examples/example-react-native-app/metro.config.js
+++ examples/example-react-native-app/metro.config.js
@@ -7,16 +7,6 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {
-  resolver: {
-    unstable_enableSymlinks: true, // this enable the use of Symlinks
-    // nodeModulesPaths: [
-    //     path.resolve(__dirname, 'node_modules'),
-    //     path.join(__dirname, '..', '..', 'js/node_modules'),
-    // ]
-  },
-  // this specifies the folder where are located the node_modules for the project
-//   watchFolders: [path.join(__dirname, '..', '..', 'js')],
-};
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
