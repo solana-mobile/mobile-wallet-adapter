1fb011419b517284ed4c9e601a0406e92a2e29fb
Matvii Mashtaliar <matvii.mashtalyar@gmail.com>
2026-03-23 12:12:44 +0100

    feat(android): add getInstalledWallets and packageName targeting to local association

--- js/packages/mobile-wallet-adapter-protocol/src/codegenSpec/NativeSolanaMobileWalletAdapter.ts
+++ js/packages/mobile-wallet-adapter-protocol/src/codegenSpec/NativeSolanaMobileWalletAdapter.ts
@@ -1,13 +1,15 @@
 import { TurboModule, TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
-    startSession(config?: { baseUri?: string }): Promise<{
+    startSession(config?: { baseUri?: string; packageName?: string }): Promise<{
         protocol_version: 'legacy' | 'v1';
     }>;
 
     invoke(method: string, params: Object | undefined): Promise<Object>;
 
     endSession(): Promise<boolean>;
+
+    getInstalledWallets(): Promise<Array<{ packageName: string; appName: string }>>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('SolanaMobileWalletAdapter') as Spec;
