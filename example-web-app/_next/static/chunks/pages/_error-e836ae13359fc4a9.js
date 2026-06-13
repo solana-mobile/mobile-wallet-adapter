3a96d55d20f351be0be7a9525e1059c74ec6fc94
Continuous Integration <>
2026-06-13 09:57:56 +0000

    Update Example Web App

---
+++ example-web-app/_next/static/chunks/pages/_error-e836ae13359fc4a9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{8452:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(2107)}])}},_=>{_.O(0,[636,593,792],()=>_(_.s=8452)),_N_E=_.O()}]);
