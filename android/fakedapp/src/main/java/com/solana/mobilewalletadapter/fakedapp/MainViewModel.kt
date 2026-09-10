d2a5eea01b9e166c47f6eca2f500b0957b790752
funkatronics <29258246+Funkatronics@users.noreply.github.com>
2026-09-10 19:51:21 +0000

    add v1 get capabilites log

--- android/fakedapp/src/main/java/com/solana/mobilewalletadapter/fakedapp/MainViewModel.kt
+++ android/fakedapp/src/main/java/com/solana/mobilewalletadapter/fakedapp/MainViewModel.kt
@@ -140,6 +140,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                 Log.d(TAG, "Capabilities: $it")
                 Log.d(TAG, "Supports legacy transactions: ${TransactionVersion.supportsLegacy(it.supportedTransactionVersions)}")
                 Log.d(TAG, "Supports v0 transactions: ${TransactionVersion.supportsVersion(it.supportedTransactionVersions, 0)}")
+                Log.d(TAG, "Supports v1 transactions: ${TransactionVersion.supportsVersion(it.supportedTransactionVersions, 1)}")
                 Log.d(TAG, "Supported features: ${it.supportedOptionalFeatures.contentToString()}")
                 showMessage(R.string.msg_request_succeeded)
             }
